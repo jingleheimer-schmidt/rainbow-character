4252fc1aab4effe4515d0a65bc6ccde19edfa913
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-10-24 22:49:16 -0700

    refactor and add additional color themes

--- settings.lua
+++ settings.lua
@@ -1,7 +1,7 @@
 
 local rainbow_speed = {
     type = "string-setting",
-    name = "rainbow-speed",
+    name = "rainbow-character-speed",
     setting_type = "runtime-per-user",
     default_value = "default",
     allowed_values = {
@@ -14,21 +14,76 @@ local rainbow_speed = {
     }
 }
 
-local rainbow_palette = {
+local rainbow_theme = {
     type = "string-setting",
-    name = "rainbow-character-palette",
+    name = "rainbow-character-theme",
     setting_type = "runtime-per-user",
     default_value = "default",
     allowed_values = {
-        "pastel",
+        -- original continuous themes
         "light",
+        "pastel",
         "default",
         "vibrant",
-        "deep"
+        "deep",
+        -- stepwise pride flag themes
+        "lesbian",
+        "gay",
+        "bi",
+        "trans",
+        "pan",
+        "ace",
+        "nonbinary",
+        "agender",
+        "progress",
+        -- stepwise country flag themes
+        "united nations",
+        "china",
+        "india",
+        "united states",
+        "indonesia",
+        "pakistan",
+        "nigeria",
+        "brazil",
+        "bangladesh",
+        "russia",
+        "mexico",
+        "japan",
+        "philippines",
+        "ethiopia",
+        "egypt",
+        "vietnam",
+        "democratic republic of the congo",
+        "turkey",
+        "iran",
+        "germany",
+        "thailand",
+        "france",
+        "united kingdom",
+        "tanzania",
+        "south africa",
+        "italy",
+        "ukraine",
+        "australia",
+        "netherlands",
+        "belgium",
+        "cuba",
+        "czech republic",
+        "greece",
+        "sweden",
     }
+}
+
+local sync_chat_color = {
+    type = "bool-setting",
+    name = "rainbow-character-sync-chat-color",
+    setting_type = "runtime-per-user",
+    default_value = false,
+    order = "a"
 }
 
 data:extend({
     rainbow_speed,
-    rainbow_palette
+    rainbow_theme,
+    sync_chat_color,
 })
