365f76465d95e4f1309c0239e030a655ca0e402a
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-10-24 19:48:01 -0700

    standardize formatting

--- control.lua
+++ control.lua
@@ -1,39 +1,40 @@
 
 local speeds = {
-  veryslow = 0.010,
-  slow = 0.025,
-  default = 0.050,
-  fast = 0.100,
-  veryfast = 0.200,
+    veryslow = 0.010,
+    slow = 0.025,
+    default = 0.050,
+    fast = 0.100,
+    veryfast = 0.200,
 }
 local palette = {
-  pastel = 1,
-  light = 64,
-  default = 128,
-  vibrant = 192,
-  deep = 255,
+    pastel = 1,
+    light = 64,
+    default = 128,
+    vibrant = 192,
+    deep = 255,
 }
 
-script.on_nth_tick(5, function(event)
-  for _, player in pairs(game.connected_players) do
-    local index = player.index
-    local nth_tick = event.nth_tick
-    local tick = event.tick
-    local frequency = 0.050
-    local player_settings = settings.get_player_settings(index)
-    local rainbow_speed = player_settings["rainbow-speed"].value
-    if rainbow_speed == "off" then
-      return
-    else
-      frequency = speeds[rainbow_speed]
+script.on_nth_tick(10, function(event)
+    for every, player in pairs(game.connected_players) do
+        if player then
+            local index = player.index
+            local nth_tick = event.nth_tick
+            local tick = event.tick
+            local frequency = 0.050
+            local player_settings = settings.get_player_settings(index)
+            local rainbow_speed = player_settings["rainbow-speed"].value
+            if rainbow_speed == "off" then
+                return
+            else
+                frequency = speeds[rainbow_speed]
+            end
+            local rainbow = {
+                r = math.sin(frequency * ((tick / nth_tick) + (index * 10)) + (0 * math.pi / 3)) * 127 + 128,
+                g = math.sin(frequency * ((tick / nth_tick) + (index * 10)) + (2 * math.pi / 3)) * 127 + 128,
+                b = math.sin(frequency * ((tick / nth_tick) + (index * 10)) + (4 * math.pi / 3)) * 127 + 128,
+                a = palette[player_settings["rainbow-character-palette"].value],
+            }
+            player.color = rainbow
+        end
     end
-    local rainbow = {
-      r = math.sin(frequency*((tick/nth_tick)+(index*10))+(0*math.pi/3))*127+128,
-      g = math.sin(frequency*((tick/nth_tick)+(index*10))+(2*math.pi/3))*127+128,
-      b = math.sin(frequency*((tick/nth_tick)+(index*10))+(4*math.pi/3))*127+128,
-      a = palette[player_settings["rainbow-character-palette"].value],
-    }
-    player.color = rainbow
-  end
-end
-)
+end)
--- settings.lua
+++ settings.lua
@@ -1,33 +1,34 @@
+
 local rainbowSpeedSetting = {
-  type = "string-setting",
-  name = "rainbow-speed",
-  setting_type = "runtime-per-user",
-  default_value = "default",
-  allowed_values = {
-    "off",
-    "veryslow",
-    "slow",
-    "default",
-    "fast",
-    "veryfast"
-  }
+    type = "string-setting",
+    name = "rainbow-speed",
+    setting_type = "runtime-per-user",
+    default_value = "default",
+    allowed_values = {
+        "off",
+        "veryslow",
+        "slow",
+        "default",
+        "fast",
+        "veryfast"
+    }
 }
 
 local rainbowPaletteSetting = {
-  type = "string-setting",
-  name = "rainbow-character-palette",
-  setting_type = "runtime-per-user",
-  default_value = "default",
-  allowed_values = {
-    "pastel",
-    "light",
-    "default",
-    "vibrant",
-    "deep"
-  }
+    type = "string-setting",
+    name = "rainbow-character-palette",
+    setting_type = "runtime-per-user",
+    default_value = "default",
+    allowed_values = {
+        "pastel",
+        "light",
+        "default",
+        "vibrant",
+        "deep"
+    }
 }
 
 data:extend({
-  rainbowSpeedSetting,
-  rainbowPaletteSetting
+    rainbowSpeedSetting,
+    rainbowPaletteSetting
 })
